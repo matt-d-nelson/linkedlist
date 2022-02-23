bec20108c2a4fbdedd438ad6ab8a719fccada1a9
matt-d-nelson <98720000+matt-d-nelson@users.noreply.github.com>
2022-02-22 18:46:40 -0600

    Initial Api Implementation

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -1,6 +1,12 @@
-package linkedlist
+//Temp package change
+package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 type Node struct {
 	Value interface{}
@@ -17,19 +23,21 @@ type LinkedList struct {
 	Tail *Node
 }
 
-func (ll *LinkedList) Add(v interface{}) {
+//Add error return
+func (ll *LinkedList) Add(v interface{}) error {
 	node := Node{Value: v}
 	if ll.Head == nil {
 		ll.Head = &node
 		ll.Tail = &node
-		return
+		return nil
 	}
 	node.Prev = ll.Tail
 	ll.Tail.Next = &node
 	ll.Tail = &node
+	return nil
 }
 
-func (ll *LinkedList) Index(i int) (Node, error) {
+func (ll *LinkedList) Read(i int) (Node, error) {
 	curr := 0
 	n := ll.Head
 	for ; curr < i && n != nil; curr++ {
@@ -39,6 +47,15 @@ func (ll *LinkedList) Index(i int) (Node, error) {
 		return Node{}, fmt.Errorf("linkedlist: %d is beyond length of list (%d)", i, curr)
 	}
 	return *n, nil
+}
+
+type Collection interface {
+	Add(v interface{}) error
+	Read(i int) (Node, error)
+}
+
+type APIQueue struct {
+	store Collection
 }
 
 func (ll *LinkedList) Reverse() {
@@ -61,4 +78,41 @@ func (ll LinkedList) String() string {
 	}
 	msg += "}"
 	return msg
+}
+
+func (q *APIQueue) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/add":
+		//Changed so all values added are strings
+		v := r.URL.Query().Get("value")
+		if err := q.store.Add(v); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]interface{}{"value added": v})
+	case "/get":
+		idx, err := strconv.Atoi(r.URL.Query().Get("index"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		v, err := q.store.Read(idx)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]interface{}{"value": v.Value})
+	default:
+		http.Error(w, fmt.Sprintf("path %v undefined", r.URL.Path), http.StatusBadRequest)
+	}
+}
+
+func main() {
+	var coll LinkedList
+	api := &APIQueue{
+		store: &coll,
+	}
+	http.ListenAndServe(":8080", api)
 }
